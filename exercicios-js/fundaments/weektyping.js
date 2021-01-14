ff54ac7c2f2ca281809fa280fe5f3c4309c9b6db
CristianoFilho <cristiano.filho@ucsal.edu.br>
2021-01-14 16:00:49 -0300

    badSmell

---
+++ exercicios-js/fundaments/weektyping.js
@@ -0,0 +1,15 @@
+let any = 'Legal'
+
+console.log(any)
+console.log(typeof any)
+
+any = 3.1516
+console.log(any)
+console.log(typeof any)
+
+//BadSmell
+
+let valor = ''
+let number = 1
+let pqp = false // exemplo badSmell
+
