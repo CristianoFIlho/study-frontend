18e77fa3c3ea808c2e6fe526010744ed68310240
CristianoFilho <cristiano.filho@ucsal.edu.br>
2021-01-16 19:09:44 -0300

    NullUnderfined and object update.

---
+++ exercicios-js/fundaments/nullUndefined.js
@@ -0,0 +1,10 @@
+let value // unassigned
+console.log(value)
+
+
+value = null //absence of value
+console.log(value)
+//console.log(value.toString()) //Error! 
+
+const product = {}
+console.log(product.value)
--- exercicios-js/fundaments/object.js
+++ exercicios-js/fundaments/object.js
@@ -2,6 +2,13 @@ const prod1 = {}
 
 prod1.name = 'Smartphone ultra mega'
 prod1.preco = 4998.90
-prod1['cool discount'] = 0.40
+prod1['cool discount'] = 0.40 //avoid attributes with space
 
 console.log(prod1)
+
+const prod2 = {
+    name: 'Polo shirt',
+    price: 79.90
+}
+prod2['cool discount'] = 0.40
+console.log(prod2)
