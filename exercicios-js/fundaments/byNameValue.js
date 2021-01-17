3d39407ce9abcd4674b7c6ebdfc7b001ae637768
CristianoFilho <cristiano.filho@ucsal.edu.br>
2021-01-17 18:01:23 -0300

    Objects exemples

---
+++ exercicios-js/fundaments/byNameValue.js
@@ -0,0 +1,26 @@
+// by name/value
+
+const salutation = 'Opa'
+
+
+function excec() {
+    const salutation = 'Hay!'
+    return salutation
+
+}
+
+//objects are aligned groups of name / value pairs 
+
+const client = {
+    name: 'Cristiano',
+    age: 22,
+    weight: 63,
+    address: {
+        publicPlace: 'Cool Street!',
+        number: 123
+    }
+}
+
+console.log(salutation)
+console.log(excec())
+console.log(client)
